bbcf0e2bf107c0b8dfebc927fed981654b39d6c8
DavidRMorphew <morphewd@umich.edu>
2020-09-11 17:08:52 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,19 +5,31 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names_array)
-    badges_array = []
-    names_array.each do |name|
-       badges_array << badge_maker(name)
+    names_array.collect do |name|
+       badge_maker(name)
     end
-    badges_array
 end
 
+# def batch_badge_creator(names_array)
+#     badges_array = []
+#     names_array.each do |name|
+#        badges_array << badge_maker(name)
+#     end
+#     badges_array
+# end
+
+# def assign_rooms(names_array)
+#     names_and_rooms_array = []
+#     names_array.each_with_index do |name, index|
+#         names_and_rooms_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+#     end
+#     names_and_rooms_array
+# end
+
 def assign_rooms(names_array)
-    names_and_rooms_array = []
-    names_array.each_with_index do |name, index|
-        names_and_rooms_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    names_array.each_with_index.collect do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index + 1}!"
     end
-    names_and_rooms_array
 end
 
 def printer(names_array)
