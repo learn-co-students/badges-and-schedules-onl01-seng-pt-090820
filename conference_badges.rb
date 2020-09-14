485eb6b6d50a4768ebef25b80eb2c717a3ee226a
Chase090 <carloyam21@icloud.com>
2020-09-14 15:44:54 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,6 @@
-# Write your code here.
+speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator (speaker) 
+    batch_badge_creator.each{|names| puts "Hello my name is #{name}" }
+    end
+end
