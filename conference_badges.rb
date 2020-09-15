528e635c7ddeb5d60feb847228cd86bbe8ef4ae9
Chase090 <carloyam21@icloud.com>
2020-09-14 17:21:47 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,33 @@
-speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator (speaker) 
-    batch_badge_creator.each{|names| puts "Hello my name is #{name}" }
-    end
+def badge_maker(name)
+  return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(people)
+    badges = []
+    people.each do |name| 
+      badges.push("Hello, my name is #{name}.")
+  end
+  return badges
+end
+
+def assign_rooms(people)
+    rooms = []
+    counter = 1
+    people.each do |name| 
+        rooms.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+        counter += 1
+    end 
+    return rooms
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |elements|
+      puts elements
+    end 
+    
+    assign_rooms(array).each do |elements|
+      puts elements 
+    end
+  end 
