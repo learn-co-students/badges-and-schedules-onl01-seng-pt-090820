8a3c4c64e840ccf387d91c2fe8dbc3c3b1bb72c1
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-15 19:13:45 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,12 +2,10 @@ def badge_maker(arel)
     badge_maker = "Hello, my name is Arel."
 end
 
-def batch_badge_creator(badge)
-  batch_badge_creator = ["Hello, my name is"]
-  badges.each do |badge|
-  batch_badge_creator += badge
+def batch_badge_creator(names)
+  
 end
-end
+
 
 def assign_rooms(arel)
 
