972f5672548ba6226e12765924b0d11359fa53a1
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-16 01:58:14 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,15 @@
-def badge_maker(arel)
-    badge_maker = "Hello, my name is Arel."
+def badge_maker(name)
+    "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(names)
-  
+    names.map { |name| badge_maker(name) }
 end
 
-
-def assign_rooms(arel)
-
+def assign_rooms(speakers)
+    new_arr = []
+    speakers.each_with_index |name, index|
+    new_arr << "Hello, #{name}! You'll be assinged to room #{index + 1}!"
+  end
+  new_arr
 end
